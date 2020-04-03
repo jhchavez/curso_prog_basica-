56259fb8419a52dde1b5f85e5162661822997a18
Jhon Chavez <jhchg7@gmail.com>
2020-04-03 00:32:13 -0400

    Ciclo while y for JS

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,9 +1,42 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 // console.log(lienzo);
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var amarillo = "yellow";
+var azul = "blue";
+var rojo = "red";
 
-dibujarLinea("black",10, 300, 220, 10)
-dibujarLinea("yellow",300, 10, 10, 220)
+// while(l < lineas)
+// {
+//     yi = 10 * l;
+//     xf = 10 * (l + 1);
+//     dibujarLinea("colorcito",0, yi, xf, 300)
+//     console.log("linea" + l);
+//     l = l + 1;
+// }
+
+for(l = 0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(amarillo,0, yi, xf, 300)
+    console.log("linea" + l);
+}
+
+for(l = 0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(azul,300, yi, xf, 0)
+    console.log("linea" + l);
+}
+
+dibujarLinea(rojo, 1,1,1,299); //izquierda
+dibujarLinea(rojo, 1,299,299,299); //abajo
+dibujarLinea(rojo, 299,1,1,1); //arriba
+dibujarLinea(rojo, 299,1,299,299); //derecha
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
