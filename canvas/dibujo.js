8eabe65e9b5f4a5d04d5f611184f01b103c1022f
Jhon Chavez <jhchg7@gmail.com>
2020-04-03 02:32:06 -0400

    Eventos y Formularios en HTML y JS

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,12 +1,32 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
 // console.log(lienzo);
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var amarillo = "yellow";
-var azul = "blue";
-var rojo = "red";
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial,yinicial);
+    lienzo.lineTo(xfinal,yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+    
+    var lineas = parseInt(texto.value);
+    var l = 0;
+    var yi, xf;
+    var amarillo = "yellow";
+    var azul = "blue";
+    var rojo = "red";
+    var espacio = ancho / lineas;
 
 // while(l < lineas)
 // {
@@ -19,31 +39,23 @@ var rojo = "red";
 
 for(l = 0; l < lineas; l++)
 {
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea(amarillo,0, yi, xf, 300)
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(rojo, 200, yi, xf, 200)
     console.log("linea" + l);
 }
 
 for(l = 0; l < lineas; l++)
 {
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea(azul,300, yi, xf, 0)
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(amarillo, 0, yi, xf, 399)
+    dibujarLinea(azul, 399, yi, xf, 0)
     console.log("linea" + l);
 }
 
-dibujarLinea(rojo, 1,1,1,299); //izquierda
-dibujarLinea(rojo, 1,299,299,299); //abajo
-dibujarLinea(rojo, 299,1,1,1); //arriba
-dibujarLinea(rojo, 299,1,299,299); //derecha
-
-function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
-{
-    lienzo.beginPath();
-    lienzo.strokeStyle = color;
-    lienzo.moveTo(xinicial,yinicial);
-    lienzo.lineTo(xfinal,yfinal);
-    lienzo.stroke();
-    lienzo.closePath();
+    dibujarLinea(rojo, 1,1,1,399); //izquierda
+    dibujarLinea(rojo, 1,399,399,399); //abajo
+    dibujarLinea(rojo, 399,1,1,1); //arriba
+    dibujarLinea(rojo, 399,1,399,399); //derecha
 }
