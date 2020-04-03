ed8f79cda512d5c278aca677ee607539977bbc1a
Jhon Chavez <jhchg7@gmail.com>
2020-04-03 19:28:50 -0400

    Detectar eventos del teclado con JavaScript

---
+++ teclas/eventos.js
@@ -0,0 +1,32 @@
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
+{
+    switch(evento.keyCode)
+    {
+        case teclas.UP:
+            console.log("arriba");
+            break;
+        case teclas.DOWN:
+            console.log("abajo")
+            break;
+        case teclas.LEFT:
+            console.log("izquierda");
+            break;
+        case teclas.RIGHT:
+            console.log("derecha");
+            break;
+            default:
+                console.log("Otra tecla");
+                break;
+    }
+}
