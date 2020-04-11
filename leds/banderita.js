1b825be71cf90491d1bf6482ccfeb2bca38ba86d
Jhon Chavez <jhchg7@gmail.com>
2020-04-11 18:43:44 -0400

    Robot para riego de plantas en Arduino, Javascript y Node

---
+++ leds/banderita.js
@@ -0,0 +1,39 @@
+var cinco = require("johnny-five");
+var circuito = new cinco.Board();
+var bombillo, motorcito, celda;
+
+circuito.on("ready", prender);
+
+function prender()
+{
+    var configuracion = {pin: "A0", freq: 50}
+    celda = new cinco.Sensor(configuracion);
+
+    bombillo = new cinco.Led(13);
+    bombillo.on();
+
+    motorcito = new cinco.Servo(9);
+    motorcito.to(90);
+
+    ondear();
+}
+
+function ondear()
+{
+    console.log("Luz: " + celda.value);
+    var luz = celda.value;
+    if(luz > 800)
+    {
+        if(turno == 1)
+        {
+            turno = 0;
+            motorcito.to(70);
+        }
+    }
+    else
+    {
+        turno = 1;
+        motorcito.to(110);
+    }
+    setTimeout(ondear, 1000);
+}
